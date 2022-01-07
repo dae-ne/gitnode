44a1383cc0d53b861d72fbae623916fac0c67e8d
derav <rafalcz29@gmail.com>
2022-01-07 10:49:47 +0100

    fix git pre-commit

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -1,8 +1,8 @@
+import path from 'path';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
 import { Configuration } from 'webpack';
 
 const config: Configuration = {
